bd3b1ec0e75e76cfb084a474225dc1a070938e6e
Per Allansson <pallansson@gmail.com>
2024-12-06 12:03:27 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#![allow(dead_code)]
+
+mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
+    println!("D1.1: {:?}", day1::part1(day1::INPUT));
+    println!("D1.2: {:?}", day1::part2(day1::INPUT));
     println!("D2.1: {:?}", day2::part1(day2::INPUT));
     println!("D2.2: {:?}", day2::part2(day2::INPUT));
     println!("D3.1: {:?}", day3::part1(day3::INPUT));
     println!("D3.2: {:?}", day3::part2(day3::INPUT));
     println!("D4.1: {:?}", day4::part1(day4::INPUT));
     println!("D4.2: {:?}", day4::part2(day4::INPUT));
+    println!("D5.1: {:?}", day5::part1(day5::INPUT));
+    println!("D5.2: {:?}", day5::part2(day5::INPUT));
 }
