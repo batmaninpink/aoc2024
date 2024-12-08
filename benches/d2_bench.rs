41479f4547c44da31287d80f4319ffa21a7ef9d8
Per Allansson <pallansson@gmail.com>
2024-12-08 11:32:18 +0100

    x

--- benches/d2_bench.rs
+++ benches/d2_bench.rs
@@ -1,9 +1,9 @@
-use aoc2024::day2;
+use aoc2024::day7::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("d2_1", |b| b.iter(|| day2::part1(black_box(day2::INPUT))));
-    c.bench_function("d2_2", |b| b.iter(|| day2::part2(black_box(day2::INPUT))));
+    c.bench_function("d2_1", |b| b.iter(|| part1(black_box(INPUT))));
+    c.bench_function("d2_2", |b| b.iter(|| part2(black_box(INPUT))));
 }
 
 criterion_group!(benches, criterion_benchmark);
