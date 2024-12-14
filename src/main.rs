95dc4a7f8345c63ce592d6d53f862a12de8b3cb5
Per Allansson <pallansson@gmail.com>
2024-12-14 15:26:56 +0100

    13

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -13,29 +14,29 @@ mod day7;
 mod day8;
 mod day9;
 
+macro_rules! day {
+    ($day:ident) => {
+        println!(
+            "{}: {},{}",
+            stringify!($day),
+            $day::part1($day::INPUT),
+            $day::part2($day::INPUT)
+        );
+    };
+}
+
 fn main() {
-    println!("D1.1: {:?}", day1::part1(day1::INPUT));
-    println!("D1.2: {:?}", day1::part2(day1::INPUT));
-    println!("D2.1: {:?}", day2::part1(day2::INPUT));
-    println!("D2.2: {:?}", day2::part2(day2::INPUT));
-    println!("D3.1: {:?}", day3::part1(day3::INPUT));
-    println!("D3.2: {:?}", day3::part2(day3::INPUT));
-    println!("D4.1: {:?}", day4::part1(day4::INPUT));
-    println!("D4.2: {:?}", day4::part2(day4::INPUT));
-    println!("D5.1: {:?}", day5::part1(day5::INPUT));
-    println!("D5.2: {:?}", day5::part2(day5::INPUT));
-    println!("D6.1: {:?}", day6::part1(day6::INPUT));
-    println!("D6.2: {:?}", day6::part2(day6::INPUT));
-    println!("D7.1: {:?}", day7::part1(day7::INPUT));
-    println!("D7.2: {:?}", day7::part2(day7::INPUT));
-    println!("D8.1: {:?}", day8::part1(day8::INPUT));
-    println!("D8.2: {:?}", day8::part2(day8::INPUT));
-    println!("D9.1: {:?}", day9::part1(day9::INPUT));
-    println!("D9.2: {:?}", day9::part2(day9::INPUT));
-    println!("D10.1: {:?}", day10::part1(day10::INPUT));
-    println!("D10.2: {:?}", day10::part2(day10::INPUT));
-    println!("D11.1: {:?}", day11::part1(day11::INPUT));
-    println!("D11.2: {:?}", day11::part2(day11::INPUT));
-    println!("D12.1: {:?}", day12::part1(day12::INPUT));
-    println!("D12.2: {:?}", day12::part2(day12::INPUT));
+    day!(day1);
+    day!(day2);
+    day!(day3);
+    day!(day4);
+    day!(day5);
+    day!(day6);
+    day!(day7);
+    day!(day8);
+    day!(day9);
+    day!(day10);
+    day!(day11);
+    day!(day12);
+    day!(day13);
 }
