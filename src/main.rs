0ea19164abea3e7d51b9213708b79f29a90388b2
Per Allansson <pallansson@gmail.com>
2024-12-15 17:44:04 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -39,4 +40,5 @@ fn main() {
     day!(day11);
     day!(day12);
     day!(day13);
+    day!(day14);
 }
